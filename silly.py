9f4c408980bedcc9634984ec967764d943582a3a
Kirill <Akavall@gmail.com>
2015-04-18 20:59:31 -0500

    fixed silly

--- silly.py
+++ silly.py
@@ -8,9 +8,3 @@ print "I am branch comment 3"
 
 print "Another branch"
 
-<<<<<<< HEAD
-print "Master master"
-=======
-print "reset 1"
-print "reset 2"
->>>>>>> f57bd93994d795c9a9942c623f4d32ce4a53c439
